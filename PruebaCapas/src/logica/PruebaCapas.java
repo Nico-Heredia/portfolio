5021843fb34b3e9f3ec332d9993bba0b60be6b4a
Nicolás Heredia <84880632+Nico-Heredia@users.noreply.github.com>
2024-02-21 11:06:07 -0300

    Add files via upload

---
+++ PruebaCapas/src/logica/PruebaCapas.java
@@ -0,0 +1,18 @@
+
+package logica;
+
+import igu.Pantalla;
+
+public class PruebaCapas {
+
+    public static void main(String[] args) {
+    
+        Pantalla panta = new Pantalla();
+        // hago visible la pantalla
+        panta.setVisible(true);
+        //par que no se abra en cuanquier lado de la pantalla
+        panta.setLocationRelativeTo(null);
+        
+    }
+    
+}
